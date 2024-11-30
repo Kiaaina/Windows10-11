306aa7725c8a0bbe4cb154572ba8732c2e805cbd
Kiaaina JohnAllen Medeiros <161550532+Kiaaina@users.noreply.github.com>
2024-11-30 12:10:29 -1000

    Add files via upload

---
+++ Mega_Play.cmd
@@ -0,0 +1,12 @@
+cd /
+title "Mega_Link_By7L"
+mode 30,2
+echo off
+color 74
+cls
+
+:top
+start https://mega.nz/folder/MK8ymYwY#qUGtS18TtmlNHY1p12r9Vg
+echo Mega_By_Kia7L...
+(timeout /t 5) > nul
+exit
